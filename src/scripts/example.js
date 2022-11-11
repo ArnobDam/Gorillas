087b488b3a6239e19f71c2766a5ad2304bf8e1c0
Arnob <arnobdam@gmail.com>
2022-11-11 11:31:19 -0500

    basic page layout

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -4,6 +4,7 @@ class Example {
         this.ele.innerHTML = "<h1>It's ALIVE!!!</h1>"
 
         this.ele.addEventListener("click", this.handleClick.bind(this));
+        //document.addEventListener("click", ...) to add event Listener to entire page
     }
 
     handleClick() {
