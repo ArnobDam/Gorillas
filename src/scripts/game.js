90c810e1ffb41489f29ad453ef02896c3b3c3aec
Arnob <arnobdam@gmail.com>
2022-11-13 01:48:09 -0500

    adjust banana starting positions

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -5,12 +5,16 @@ let canvasWidth = window.innerWidth * .65;
 let canvasHeight = window.innerHeight * .65;
 
 const banana1 = new Banana({
-    pos: [canvasWidth / 10, canvasHeight * (4 / 5)],
+    pos: [(canvasWidth) * (16.5 / 200), canvasHeight * (75 / 100)],
+    //16.5 = gorilla1 starting position + half gorilla width
+    //75 same as gorilla1 starting height
     vel: [1.5, -2],
     color: '#F6BE00' //yellow in hex 
 });
 const banana2 = new Banana({
-    pos: [canvasWidth * (9 / 10), canvasHeight * (4 / 5)],
+    pos: [canvasWidth * (183.5 / 200), canvasHeight * (75 / 100)],
+    //183.5 = gorilla2 starting position + half gorilla width
+    //75 same as gorilla2 starting height
     vel: [-1.5, -2.5],
     color: '#F67E80'
 });
@@ -46,7 +50,8 @@ Game.prototype.draw = function (context) {
             banana1.draw(context);
         } else {
             this.turn = 2; //switch turn
-            banana1.pos = [canvasWidth / 10, canvasHeight * (4 / 5)]; //reset player's banana pos
+            banana1.pos = [(canvasWidth) * (16.5 / 200), canvasHeight * (75 / 100)];
+            //reset player's banana pos
             banana1.vel = [1.5, -2]; //reset player's banana vel
             banana1.gravityY = 0; //reset player's banana gravity
         }
@@ -57,7 +62,8 @@ Game.prototype.draw = function (context) {
             banana2.draw(context);
         } else {
             this.turn = 1; //switch turn
-            banana2.pos = [canvasWidth * (9 / 10), canvasHeight * (4 / 5)]; //reset opponent's banana pos
+            banana2.pos = [canvasWidth * (183.5 / 200), canvasHeight * (75 / 100)];
+            //reset opponent's banana pos
             banana2.vel = [-1.5, -2.5] //reset opponent's banana vel
             banana2.gravityY = 0; //reset opponent's banana gravity
         }
