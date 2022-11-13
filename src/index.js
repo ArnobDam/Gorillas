a9aed5cd89c0ff4d83a3174e153c6e75f3f53b6d
Arnob <arnobdam@gmail.com>
2022-11-13 00:46:37 -0500

    working automatic turns

--- src/index.js
+++ src/index.js
@@ -11,39 +11,13 @@ const GameView = require("./scripts/game_view.js");
 let canvasWidth = window.innerWidth * .65;
 let canvasHeight = window.innerHeight * .65;
 
-const Banana = require("./scripts/banana.js");
-const Gorilla = require("./scripts/gorilla.js");
-
-const banana1 = new Banana({
-    pos: [canvasWidth / 10, canvasHeight * (4 / 5)],
-    vel: [1.5, -2],
-    color: '#F6BE00' //yellow in hex 
-});
-const banana2 = new Banana({
-    pos: [canvasWidth * (9 / 10), canvasHeight * (4 / 5)],
-    vel: [-1.5, -2.5],
-    color: '#F67E80'
-});
-// window.Banana = Banana;
-
-const gorilla1 = new Gorilla({
-    pos: [canvasWidth / 20, canvasHeight * (4 / 5)],
-    color: '#000000'
-});
-
-const gorilla2 = new Gorilla({
-    pos: [canvasWidth * (17 / 20), canvasHeight * (4 / 5)],
-    color: '#964B00'
-});
-
-let turn = 1;
 
 document.addEventListener("DOMContentLoaded", function () {
 
     const canvasEle = document.getElementsByTagName("canvas")[0];
 
-    canvasEle.width = window.innerWidth * .65;
-    canvasEle.height = window.innerHeight * .65;
+    canvasEle.width = canvasWidth;
+    canvasEle.height = canvasHeight;
 
     const context = canvasEle.getContext("2d");
 
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -26,7 +26,7 @@ const gorilla2 = new Gorilla({
     color: '#964B00'
 });
 
-let turn = 1;
+this.turn = 1;
 
 function Game() { }
 
@@ -34,16 +34,32 @@ Game.prototype.draw = function (context) {
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     gorilla1.draw(context);
     gorilla2.draw(context);
-    if (turn === 1) {
+    if (this.turn === 1) {
         if (!banana1.outOfBounds()) {
             banana1.draw(context);
+        } else {
+            this.turn = 2; //switch turn
+            banana1.pos = [canvasWidth / 10, canvasHeight * (4 / 5)]; //reset player's banana pos
+            banana1.vel = [1.5, -2]; //reset player's banana vel
+            banana1.gravityY = 0; //reset player's banana gravity
         }
     } else {
         if (!banana2.outOfBounds()) {
             banana2.draw(context);
+        } else {
+            this.turn = 1; //switch turn
+            banana2.pos = [canvasWidth * (9 / 10), canvasHeight * (4 / 5)]; //reset opponent's banana pos
+            banana2.vel = [-1.5, -2.5] //reset opponent's banana vel
+            banana2.gravityY = 0; //reset opponent's banana gravity
         }
     }
 }
 
+
+// Game.prototype.hasCollided = function () {
+//     if (turn === 1) {
+
+//     }
+// }
 
 module.exports = Game;
