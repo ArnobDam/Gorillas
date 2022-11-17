babd65669784760fdf46307d82170b91bee53e01
Arnob <arnobdam@gmail.com>
2022-11-17 11:23:40 -0500

    gameview  properly resizing when screen changes size

--- src/scripts/game_view.js
+++ src/scripts/game_view.js
@@ -11,6 +11,17 @@ window.HIT_TICK = 0;
 
 GameView.prototype.animate = function animate() {
 
+    console.log(window.innerWidth);
+
+    let canvasWidth = window.innerWidth * .65;
+    let canvasHeight = window.innerHeight * .65;
+
+    const canvasEle = document.getElementsByTagName("canvas")[0];
+
+    canvasEle.width = canvasWidth;
+    canvasEle.height = canvasHeight;
+
+
     this.game.draw(this.context);
 
     TICK++;
